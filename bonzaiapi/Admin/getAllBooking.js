737d227196ca5ac4980454bdb30c248bd795077e
Jonathan Tirapegui <137382571+damiantirapegui@users.noreply.github.com>
2024-09-09 13:34:28 +0200

    Updated yml file

---
+++ bonzaiapi/Admin/getAllBooking.js
@@ -0,0 +1,8 @@
+exports.handler = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "Go Serverless v4! Your function executed successfully!",
+    }),
+  };
+};
