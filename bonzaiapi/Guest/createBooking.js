b39e96c7db6f6530d82c7c1907a80858dcaf4a64
Simon Gustavsson <simgus1234@gmail.com>
2024-09-09 21:11:05 +0200

    Push last version

--- bonzaiapi/Guest/createBooking.js
+++ bonzaiapi/Guest/createBooking.js
@@ -4,26 +4,64 @@ const docClient = new AWS.DynamoDB.DocumentClient({
   region: "eu-north-1",
 });
 
+const checkOccupiedRooms = async () => {
+  const tableName = "allBookings";
+  try {
+    const data = await docClient.scan({ TableName: tableName }).promise();
+
+    const totalOccupiedRooms = data.Items.reduce((acc, item) => {
+      const { totalRooms } = item;
+      return acc + (totalRooms || 0); // Loopa samtliga bokningar och summera totalRooms
+    }, 0);
+
+    // Returnera antal upptagna rum
+    return totalOccupiedRooms;
+  } catch (error) {
+    console.error("Hittade inga rum:", error);
+    throw new Error("Ooops, något gick fel. Försök igen.");
+  }
+};
+
+const calculateNights = (startDate, endDate) => {
+  const start = new Date(startDate);
+  const end = new Date(endDate);
+
+  if (end < start) {
+    throw new Error(
+      "Din bokning är incorrect. Var vänlig och kontrollera dina val."
+    );
+  }
+
+  // Millisekunder mellan start och slut
+  const differenceInMillis = end - start;
+
+  // Konvertera till dagar
+  const millisecondsInDay = 24 * 60 * 60 * 1000;
+  const differenceInDays = differenceInMillis / millisecondsInDay;
+
+  // Returnera antal nätter för bokningen
+  return differenceInDays;
+};
+
 exports.handler = async (event) => {
   const tableName = "allBookings";
 
-  // Parsing event body
-  const body = JSON.parse(event.body); // No need for await here
+  const body = JSON.parse(event.body);
 
-  console.log("Body: ", body);
+  //Kontrollera lediga rum
+  const freeRooms = 20 - (await checkOccupiedRooms());
 
-  const { bookingName, guests, roomsReq } = body;
+  // Data från clienten
+  const { bookingName, guests, roomsReq, from, to } = body;
 
-  console.log("Booking name: ", bookingName);
-  console.log("Guests: ", guests);
-  console.log("Rooms requested: ", roomsReq);
+  //Slumpa ett unikt id för bokningen. Används vid avbokning eller kontakt med support.
   const id = uuidv4();
 
+  // Räkna antal olika typer av rum
   let svit = 0;
   let dubbel = 0;
   let single = 0;
 
-  // Process room types and counts
   roomsReq.forEach((room) => {
     switch (room.name) {
       case "Svit":
@@ -38,15 +76,34 @@ exports.handler = async (event) => {
     }
   });
 
-  // Calculate cost and room details
-  const cost = svit * 1500 + dubbel * 1000 + single * 500;
+  const nights = calculateNights(from, to);
+
+  // Räkna ut kostnaden och antal rum som krävs
+  const cost =
+    svit * 1500 * nights + dubbel * 1000 * nights + single * 500 * nights;
   const totalBeds = svit * 3 + dubbel * 2 + single * 1;
   const totalRooms = svit + dubbel + single;
 
+  // Kontrollera att det finns tillräckligt med rum
+  if (totalRooms > freeRooms) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: `Tyvärr finns det inte tillräckligt med rum för din bokning. Vi har endast ${freeRooms} rum kvar. Din bokning kräver ${totalRooms} rum.`,
+      }),
+    };
+  }
+
   // Validate booking
-  if (totalBeds >= guests && guests !== 0) {
+  if (
+    totalBeds >= guests &&
+    guests !== 0 &&
+    !totalRooms <= guests &&
+    from &&
+    to
+  ) {
     try {
-      // Save booking to DynamoDB
+      // Spara till databasen
       await docClient
         .put({
           TableName: tableName,
@@ -56,7 +113,9 @@ exports.handler = async (event) => {
             guests: guests,
             totalRooms: totalRooms,
             totalBeds: totalBeds,
-            cost: cost, // This is a number, not a string
+            cost: cost,
+            from: from,
+            to: to,
           },
         })
         .promise();
@@ -64,29 +123,34 @@ exports.handler = async (event) => {
       return {
         statusCode: 200,
         body: JSON.stringify({
-          message: `Bokning skapad med id: ${id}. Du har bokat ${svit} sviter, ${dubbel} dubbelrum och ${single} enkelrum. Totalt antal gäster: ${guests}. Kostnad: ${cost} kr`,
+          message: `Tack för din bokning, ${bookingName}! Vi ser fram emot att välkomna dig till Bonzai Hotel mellan ${from} och ${to}. Ditt bokningsnummer är ${id}. Använd detta nummer vid avbokning eller kontakt med support.  Total kostnad för din bokning är ${cost} kr. Vid eventuella frågor, vänligen kontakta oss på Bonzai Hotels!.`,
           bookingName: bookingName,
           guests: guests,
         }),
       };
     } catch (error) {
-      // Log and return 500 in case of error
       console.error("DynamoDB Error: ", error);
       return {
         statusCode: 500,
         body: JSON.stringify({
-          message: "Could not create booking",
+          message: "Bokningen misslyckades. Försök igen!",
           error: error.message,
         }),
       };
     }
-  } else {
-    // Validation failure response
+  } else if (guests < totalRooms) {
     return {
       statusCode: 400,
       body: JSON.stringify({
         message:
-          "Ojsan. Det verkar som att du har bokat för många gäster. Försök igen.",
+          "Oj då, antalet gäster är färre än antalet rum. Var vänlig och kontrollera dina val.",
+      }),
+    };
+  } else {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: "Oj då, något gick fel. Var vänlig och kontrollera dina val.",
       }),
     };
   }
