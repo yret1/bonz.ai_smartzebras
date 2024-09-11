d7a4b8ad672df88f1ef588aca55831d51080202f
Jonathan Tirapegui <137382571+damiantirapegui@users.noreply.github.com>
2024-09-11 10:55:06 +0200

    readME

--- bonzaiapi/Guest/deleteBooking.js
+++ bonzaiapi/Guest/deleteBooking.js
@@ -87,26 +87,3 @@ exports.handler = async (event) => {
     };
   }
 };
-
-// const calculateRemovable = (startDate) => {
-//   //Bokningens startdatum
-//   // const start = new Date(startDate);
-//   // //Dagens datum
-//   // const today = new Date();
-//   // if (today > start) {
-//   //   throw new Error(
-//   //     "Din bokning är redan aktiv. Det går inte längre att ändra eller avboka."
-//   //   );
-//   // }
-//   // // Millisekunder mellan start och slut
-//   // const differenceInMillis = start - today;
-//   // // Konvertera till dagar
-//   // const millisecondsInDay = 24 * 60 * 60 * 1000;
-//   // const differenceInDays = differenceInMillis / millisecondsInDay;
-//   // // Returnera antal nätter för bokningen
-//   // if (differenceInDays >= 2) {
-//   //   return true;
-//   // } else {
-//   //   return false;
-//   // }
-// };
